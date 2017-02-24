7ca894c5fee4da2c5e2d435a41430aa664d4777f
wilfalken <tassin.guillaume.56@gmail.com>
2017-02-24 14:14:25 +0100

    protectd -> public in DAO's

--- DAO/CategorieDAO.php
+++ DAO/CategorieDAO.php
@@ -3,24 +3,56 @@
 class CategorieDAO extends DAO{
     
     private $table = "categorie";
-    private $clePrimaire = "id_categorie";
+    private $clePrimaire = "nom_categorie";
 
     public function create($obj) {
         
+        $nomCat=$obj->getNomCat();
+        
+    	$req = Connexion::getInstance()->prepare("INSERT INTO ".$this->table." (nom_cat)". "VALUES (?)");
+       	$req->bindParam(1, $nomCat);    	
+    	$req->execute();        
+    }
 
+        
+    protected function delete($obj) {
+        $nom_categorie=$obj->getNomCat();
+        $stmt = Connexion::getInstance()->prepare("DELETE FROM ".$this->table." WHERE ".$this->clePrimaire." = ".$nom_categorie.";");
+        $stmt->execute();
+    }
+
+    protected function find($id) {
+        $stmt = Connexion::getInstance()->prepare("SELECT * FROM ".$this->table." WHERE ".$this->clePrimaire." = ".$id.";");
+        $stmt->execute();
+        $d = $stmt->fetch();
+        $categorie = new categorie($d["nom_categorie"]);
+            
+        return $categorie;
         
     }
 
-    public function delete($obj) {
+
+    protected function update($obj) {
+        $stmt = Connexion::getInstance()->prepare("UPDATE ".$this->table." SET nom_categorie='?' ; ");
+        
+        $stmt->bindParam(1, $obj->getnomCat());
+        
+        $stmt->execute(); 
         
     }
-
-    public function find($id) {
+    
+        protected function categorieExist($obj){
+        $succes=false;
         
-    }
-
-    public function update($obj) {
+        $categorieCourant=$obj->getnomCat();
         
+        $objAComparer=self::find($categorieCourant);
+        $categorieAComparer=$objAComparer->getPseudo();
+        
+        if($categorieCourant==$objAComparer){
+            $succes=true;
+        }
+        return $succes;
     }
 
 }
--- Modele/Categorie.php
+++ Modele/Categorie.php
@@ -2,17 +2,26 @@
 
 class Categorie {
    
-    private $table = "categorie";
-    private $clePrimaire = "nom_categorie";
-
+    private $table = "Categorie";
+    private $clePrimaire = "id_categorie";
+    
+    private $idCat;
     private $nomCat;
     
     function __construct($nomCat) {
         $this->nomCat = $nomCat;
     }
 
+    function getIdCat() {
+        return $this->idCat;
+    }
+
     function getNomCat() {
         return $this->nomCat;
+    }
+
+    function setIdCat($idCat) {
+        $this->idCat = $idCat;
     }
 
     function setNomCat($nomCat) {

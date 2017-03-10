bbb5f960f5bfd5230eaef67dc87782cc66b88ac0
wilfalken <tassin.guillaume.56@gmail.com>
2017-03-10 12:27:40 +0100

    slider fini !!! correction table propres jeux...

--- Controleur/c_admin_evenements.php
+++ Controleur/c_admin_evenements.php
@@ -1,11 +1,17 @@
 <?php
 
-//if(!isset($_POST['action'])){
-//	$_POST['action'] = 'evenement';
-//}
+if(!isset($_POST['action'])){
+	$_POST['action'] = 'evenement';
+}
 
 $action = $_POST['action'];
 switch ($action) {
+    case "affichageEven":
+        $id = $_POST['id'];
+        $daoeven = new EvenementDAO();
+        $even = $daoeven->find($id);
+        include_once 'Vue/v_evenement.php';
+        break;
     case "ajouterEvenement":
         $id = $_POST['id'];
         $daoeven = new EvenementDAO();
